e2fe72c3ae3e10194335af4cfdd92de81c949088
Manoj Murlidhar Naik <naikmanoj1991@gmail.com>
2019-02-03 22:40:47 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.CodeDom.Compiler;
+
+namespace SoloLearnChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter Variable name : ");
+            var variableName = Console.ReadLine().ToString();
+
+            Console.WriteLine(IsValidVarName(variableName));
+        }
+
+        /// <summary>
+        /// Varibale name validation
+        /// </summary>
+        /// <param name="variableName">Variablename</param>
+        /// <returns>
+        /// "Valid" if varibale is valid
+        /// "Invalid" if varibale is invalid
+        /// </returns>
+        private static string IsValidVarName(string variableName)
+        {
+            CodeDomProvider provider = CodeDomProvider.CreateProvider("C#");
+
+            if (provider.IsValidIdentifier(variableName))
+            {
+                return "Valid";
+            }
+            else
+            {
+                return "Invalid";
+            }
+        }
+    }
+}
