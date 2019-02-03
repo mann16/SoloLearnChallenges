f90fb0b4c76c5efd775f89a1e70b92162c4fc458
Manoj Murlidhar Naik <naikmanoj1991@gmail.com>
2019-02-03 23:00:30 +0530

    Code related to ASCII Challenge added

--- Program.cs
+++ Program.cs
@@ -7,11 +7,16 @@ namespace SoloLearnChallenges
     {
         static void Main(string[] args)
         {
-            
+
+            Console.WriteLine("Varibale name validatioon challange");
             Console.WriteLine("Enter Variable name : ");
             var variableName = Console.ReadLine().ToString();
-
             Console.WriteLine(IsValidVarName(variableName));
+
+            Console.WriteLine("ASCII Converter : ");
+            Console.WriteLine("Enter text : ");
+            var text = Console.ReadLine().ToString();
+            Console.WriteLine(ASCIIConverer(text));
         }
 
         /// <summary>
@@ -34,6 +39,33 @@ namespace SoloLearnChallenges
             {
                 return "Invalid";
             }
+        }
+
+        /// <summary>
+        /// Convert text to ASCII
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns>ASCII String</returns>
+        private static string ASCIIConverer(string text)
+        {
+            string asciiText = string.Empty;
+            foreach (var c in text)
+            {
+                int unicode = c;
+
+                if (unicode < 128)
+                {
+                    asciiText += string.Format("{0} ", unicode);
+                }
+                else
+                {
+                    asciiText = "Provided text is not valid";
+                }
+                
+            }
+
+            return asciiText;
+            ;
         }
     }
 }
